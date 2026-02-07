4fdb79c91bdc1bf497d3bf6b810b247949e5b0e6
ControlAltern <soham@chugul.com>
2026-02-07 11:23:46 +0530

    finish main.py
    finished main.py, will add ultrasonic functionality later

--- src/main.py
+++ src/main.py
@@ -8,3 +8,17 @@ CORS(
     resources={r'api/*': {"origins": "http:localhost:5173"}}
 )
 
+@app.route("/")
+def home():
+    return "flask runs bro"
+
+@app.route("/api/data")
+def get_data():
+    return jsonify(
+        wetOriginalDepth=500,
+        wetCurrentDepth=345,
+        dryOriginalDepth=5000,
+        dryCurrentDepth=851
+    )
+
+app.run(debug=True)
